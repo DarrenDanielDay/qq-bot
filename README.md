210e24c27ceeaa175478058629c5b4c882a3d57d
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-06-22 03:59:21 +0800

    dev: 添加配置输入

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# taio
+# qq-bot
 
-TypeScript All In One.
+基于[`go-cqhttp`](https://github.com/Mrs4s/go-cqhttp)的 nodejs 实现逻辑的 qq 机器人
 
-> Still in development.
+## 环境要求
+
+`vscode`的`code`命令可用（主要用于打开二维码）
+`yarn` >= 1.22
+
+## 快速开始
+
+```sh
+yarn        # 安装依赖
+yarn start  # 运行机器人
+```
+
+> 功能模块开发中
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,110 @@
 import * as cq from "go-cqwebsocket";
 import * as child_process from "child_process";
 import * as path from "path";
-function startCQ(): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const exe = path.resolve(process.cwd(), "bin", "go-cqhttp");
-    const cqProcess = child_process.spawn(`${exe}`, {
-      cwd: path.resolve(process.cwd(), "bin"),
+import * as util from "util";
+import * as fs from "fs";
+import { createInterface } from "readline";
+
+const read = util.promisify(fs.readFile);
+const write = util.promisify(fs.writeFile);
+const access = util.promisify(fs.access);
+const cwd = process.cwd();
+function getLineInput() {
+  const readline = createInterface({ input: process.stdin });
+  return new Promise<string>((resolve) => {
+    readline.once("line", (line) => {
+      resolve(line);
+      readline.close();
     });
-    cqProcess.stdout.addListener("data", (chunk) => {
-      if (Buffer.isBuffer(chunk)) {
-        const out = chunk.toString("utf-8");
-        if (out.includes("127.0.0.1")) {
-          resolve();
-        }
+  });
+}
+async function createConfig() {
+  console.log("正在检查配置");
+  const configPath = path.resolve(cwd, "bin", "config.yml");
+  try {
+    await access(configPath);
+  } catch {
+    console.log("未找到配置，请输入QQ号，并用手机扫码登录");
+    const qq = await getLineInput();
+    // 生成配置
+    const templatePath = path.resolve(
+      process.cwd(),
+      "bin",
+      "config-template.yml"
+    );
+    const template = await read(templatePath, { encoding: "utf-8" });
+    const config = template.replace('"QQ Number"', qq);
+    await write(configPath, config);
+  }
+}
+
+function chunkToString(chunk: unknown) {
+  if (Buffer.isBuffer(chunk)) {
+    return chunk.toString("utf-8");
+  }
+  return "" + chunk;
+}
+
+function logChunk(chunk: unknown) {
+  console.log(chunkToString(chunk));
+}
+
+function startCQ() {
+  console.log("正在创建go-cqhttp后台进程");
+  const exe = path.resolve(cwd, "bin", "go-cqhttp.exe");
+  const cqProcess = child_process.spawn(`${exe}`, {
+    cwd: path.resolve(cwd, "bin"),
+  });
+  cqProcess.stdout.addListener("data", logChunk);
+  cqProcess.stderr.addListener("data", logChunk);
+  return new Promise<void>((resolve) => {
+    const resolveWhenReady: (chunk: any) => void = (chunk) => {
+      if (chunkToString(chunk).includes("127.0.0.1")) {
+        resolve();
       }
-    });
-    cqProcess.on("close", reject);
-    cqProcess.on("error", reject);
-    cqProcess.on("exit", reject);
-    process.stdin.pipe(cqProcess.stdin);
-    cqProcess.stdout.pipe(process.stdout);
+    };
+    cqProcess.stdout.addListener("data", resolveWhenReady);
+    cqProcess.stderr.addListener("data", resolveWhenReady);
   });
 }
 
-function bot() {
+async function startBot() {
+  console.log("正在启动机器人");
   const bot = new cq.CQWebSocket({});
+  bot.connect();
+  registerBotHandlers(bot);
+}
+
+async function registerBotHandlers(bot: cq.CQWebSocket) {
   bot.on("message.group", ({ context: { group_id, message } }) => {
     console.log(group_id, message);
   });
+  bot.on("message.private", ({ context: { message, user_id } }) => {
+    console.log(user_id, message);
+  });
+  bot.on("socket.close", () => {
+    console.log("socket连接关闭");
+  });
 }
 
-async function main() {
-  await startCQ();
-  bot();
+function startQRCodeWatcher() {
+  const interval = setInterval(async () => {
+    const qrCodePath = path.resolve(cwd, "bin", "qrcode.png");
+    try {
+      await access(qrCodePath);
+    } catch (error) {
+      return;
+    }
+    clearInterval(interval);
+    setTimeout(() => {
+      child_process.execSync(`code ${qrCodePath}`);
+    }, 1000);
+  }, 1000);
 }
-main()
-  .catch(console.error)
-  .finally(() => {
-    console.log("Bot has been shut down.");
-  });
+async function main() {
+  await createConfig();
+  startQRCodeWatcher();
+  await startCQ();
+  startBot();
+}
+main();
